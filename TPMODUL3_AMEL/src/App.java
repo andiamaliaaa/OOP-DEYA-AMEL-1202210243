7a96dcb621febb8e5ed49e67cdde511076baefd9
andiamaliaaa <115783285+andiamaliaaa@users.noreply.github.com>
2022-12-24 14:55:37 +0700

    Add files via upload

---
+++ TPMODUL3_AMEL/src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Restaurant restoran = new Restaurant();
+        int customerID, orderQty;
+        try {   
+            System.out.print("Enter Customer ID: ");
+            customerID = in.nextInt();
+    
+            System.out.print("Enter how much food to made: ");
+            orderQty = in.nextInt();
+    
+            Thread t1 = new Thread(restoran);
+            Waiters waiter = new Waiters(customerID, orderQty);
+            Thread t2 = new Thread(waiter);
+    
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+    
+        } catch (Exception e) {
+            System.out.println("Input must be Integer");
+        }
+    
+    }
+    
+}
