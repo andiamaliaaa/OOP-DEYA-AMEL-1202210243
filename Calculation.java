3bef19307421a4ede89140753332d6155b2ab7f6
andiamaliaaa <115783285+andiamaliaaa@users.noreply.github.com>
2022-12-31 07:47:20 +0700

    MODUL3_AMEL

---
+++ Calculation.java
@@ -0,0 +1,52 @@
+public class Calculation implements Runnable{
+    public double radius, side, area;
+    public double phi = 3.14;
+
+    public void setSquare(double side){
+        if(side >= 1){
+            this.side = side;
+            this.area = side*side;
+        }else{
+            throw new IllegalArgumentException("Side must be greater than 1");
+        }
+    }
+
+    public double getSquare(){
+        return area;
+    }
+
+    public void setCircle(double radius){
+        if(radius >= 1){
+            this.radius = radius;
+            this.area = phi*radius*radius;
+        }else{
+            throw new IllegalArgumentException("Radius must be greater than 1");
+        }
+    }
+
+    public double getCircle(){
+        return area;
+    }
+
+    public void setTrapezoid(double a, double b, double t){
+        this.area = 0.5*(t*(a+b));
+    }
+
+    public double getTrapezoid(){
+        return area;
+    }
+
+    @Override
+    public void run(){
+        System.out.println();
+        System.out.println("====Program will start in====");
+        for (int i = 3; i >= 1; i--){
+            System.out.println("Starting with thread "+i);
+        }
+        try{
+            Thread.sleep(5000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
